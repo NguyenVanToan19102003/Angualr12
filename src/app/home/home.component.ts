7c0a80b715528270f8653854f622f4161bd9609d
DESKTOP-4SFC0M1\HP <toannvph25194@fpt.edu.vn>
2023-09-29 15:05:26 +0700

    ngtemplate viết if , else then trong html

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
     public name = 'Toản Nguyễn'
-
+    public age = 18;
 
     public resetName() :void {
          console.log('resetName123');
