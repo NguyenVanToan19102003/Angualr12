53aef618c7e6be7a8fbdc198526aa824f1257284
DESKTOP-4SFC0M1\HP <toannvph25194@fpt.edu.vn>
2023-09-30 21:16:14 +0700

    *ngFor and *ngClass

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,9 +8,19 @@ import { Component } from '@angular/core';
 export class HomeComponent {
     public name = 'Toản Nguyễn'
     public age = 18;
+    public traiCay = ['Táo', 'Cam', 'Nho', 'Xoài', 'Ổi'];
+    public traiCay2 = [
+      {name : 'Táo', price : 12 , giamGia : false},
+      {name : 'Cam', price : 2 , giamGia : true},
+      {name : 'Ổi', price : -1 , giamGia : true}
+    ];
 
     public resetName() :void {
          console.log('resetName123');
          this.name = '';
     }
+    public ngOnInit(): void {
+      console.log(' trai cay =', this.traiCay);
+    }
 }
+
